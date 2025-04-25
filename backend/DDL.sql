43429d9051951fbf325636276b9004eb665a5ab3
sreyas <sreyas35741236@gmail.com>
2025-04-25 11:14:38 +0530

    setup done

---
+++ backend/DDL.sql
@@ -0,0 +1,73 @@
+DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS user_book_reviews CASCADE;
+DROP TABLE IF EXISTS book_genres CASCADE;
+DROP TABLE IF EXISTS books CASCADE;
+DROP TABLE IF EXISTS genres CASCADE;
+DROP TABLE IF EXISTS follows CASCADE;
+DROP TABLE IF EXISTS Users CASCADE;
+
+-- user info - username, email, password
+CREATE TABLE Users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+-- followers - user_id, follower_id
+CREATE TABLE follows (
+    follower_id INTEGER NOT NULL,
+    following_id INTEGER NOT NULL,
+    followed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (follower_id, following_id),
+    FOREIGN KEY (follower_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (following_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    CHECK (follower_id <> following_id)
+);
+
+--  genres
+CREATE TABLE genres (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+
+
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    avg_rating DECIMAL(3,2) CHECK (avg_rating >= 0 AND avg_rating <= 5),
+    num_ratings INT DEFAULT 0,
+    num_reviews INT DEFAULT 0,
+    description VARCHAR(200) CHECK (LENGTH(description) <= 200)  -- Limit to 200 characters
+);
+
+CREATE TABLE book_genres (
+    book_id INT REFERENCES books(id) ON DELETE CASCADE,
+    genre_id INT REFERENCES genres(id) ON DELETE CASCADE,
+    PRIMARY KEY (book_id, genre_id)
+);
+
+CREATE TABLE user_book_reviews (
+    review_id SERIAL PRIMARY KEY,
+    book_id INTEGER REFERENCES books(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+    rating INTEGER NOT NULL CHECK (rating BETWEEN 1 AND 5),
+    comment TEXT NULL,
+    updated_at TIMESTAMP DEFAULT NOW(),
+    UNIQUE (book_id, user_id)  -- Ensures one review per user per book
+);
+
+
+
+
+
+CREATE TABLE messages (
+    message_id SERIAL PRIMARY KEY,
+    sender_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+    recipient_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
